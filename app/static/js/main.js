d58a312b8ad975b993133f657b67b74452618fcd
jshwi <stephen@jshwisolutions.com>
2021-10-19 13:22:42 +1100

    doc: Documents main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,26 @@
+/**
+ * main.js
+ * =======
+ * [Sourced functions for HTML scripts.]
+ */
+
+/**
+ * [Set the count for number of messages held.]
+ * @function set_message_count
+ * @param n
+ */
 function set_message_count(n) {
   let message_count = $("#message_count");
   message_count.text(n);
   message_count.css("visibility", n ? "visible" : "hidden");
 }
+
+/**
+ * [Set the percentage to show for task progress.]
+ * @function set_task_progress
+ * @param {String} task_id
+ * @param {String} progress
+ */
 function set_task_progress(task_id, progress) {
   $("#" + task_id + "-progress").text(progress);
 }
