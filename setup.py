fb4752eb320723015f183c94d1b86c45692d106b
jshwi <stephen@jshwisolutions.com>
2021-11-08 14:34:40 +1100

    Bump version: 1.8.0 → 1.8.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.rst") as file:
 
 setup(
     name="jss",
-    version="1.8.0",
+    version="1.8.1",
     description="A Flask web application",
     long_description=README,
     long_description_content_type="text/x-rst",
