2bcff0336d7ba03a5811fea2932128011b7f45af
jshwi <stephen@jshwisolutions.com>
2021-11-16 01:12:14 +1100

    chore: Updates setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         "gunicorn==20.1.0",
         "itsdangerous==2.0.1",
         "markupsafe==2.0.1",
-        "psycopg2==2.9.1",
+        "psycopg2==2.9.2",
         "pyjwt==2.3.0",
         "redis==3.5.3",
         "rq==1.10.0",
