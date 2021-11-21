a8dc2efdac50ce9181ea9943e575cbb0b61b861b
jshwi <stephen@jshwisolutions.com>
2021-11-21 21:17:01 +1100

    refactor: Refactors `app.dom.renderers`

--- app/dom/renderers.py
+++ app/dom/renderers.py
@@ -30,15 +30,15 @@ class IconView(View):
         super().__init__(text, endpoint, **kwargs)
         self.text = text
         self.title = text
-        self.aclass = ""
+        self.cls = ""
 
     def set_icon(self, icon: str) -> None:
         """Override attributes to render an icon instead of text.
 
         :param icon: Icon attribute.
         """
-        self.text = tags.span(_class=icon)
-        self.aclass = "btn-lg btn-link"
+        self.text = tags.span(cls=icon)
+        self.cls = "btn-lg btn-link"
 
 
 class BadgedView(IconView):
@@ -157,48 +157,51 @@ class NavbarRenderer(Visitor):
         :return: Rendered HTML.
         """
         node_id = sha1(str(id(node)).encode()).hexdigest()
-        root = tags.nav(_class="navbar navbar-default")
-        cont = root.add(tags.div(_class="container-fluid"))
-        header = cont.add(tags.div(_class="navbar-header"))
+        nav = tags.nav(cls="navbar navbar-default")
+        container = nav.add(tags.div(cls="container-fluid"))
 
-        # collapse button
-        # this will collapse all nav items containing the ``node_id`` on
-        # mobile devices
-        btn = header.add(tags.button())
-        btn["type"] = "button"
-        btn["class"] = "navbar-toggle collapsed"
-        btn["data-toggle"] = "collapse"
-        btn["data-target"] = f"#{node_id}"
-        btn["aria-expanded"] = "false"
-        btn["aria-controls"] = "navbar"
-        btn.add(tags.span("Toggle navigation", _class="sr-only"))
-        btn.add(tags.span(_class="icon-bar"))  # "hamburger" menu
-        btn.add(tags.span(_class="icon-bar"))
-        btn.add(tags.span(_class="icon-bar"))
+        # header; div2
+        div_header = container.add(tags.div(cls="navbar-header"))
+        collapse_button = div_header.add(
+            tags.button(
+                type="button",
+                cls="navbar-toggle collapsed",
+                data_toggle="collapse",
+                data_target=f"#{node_id}",
+                aria_expanded="false",
+                aria_controls="navbar",
+            )
+        )
+        collapse_button.add(
+            tags.span("Toggle navigation", cls="sr-only"),
+            tags.span(cls="icon-bar"),  # "hamburger" menu
+            tags.span(cls="icon-bar"),
+            tags.span(cls="icon-bar"),
+        )
 
-        # dark mode toggle
-        label = header.add(tags.label(_class="switch pull-right"))
+        label = div_header.add(tags.label(cls="switch pull-right"))
         label.add(*[self.visit(i) for i in node.header])
 
-        # brand
-        brand = header.add(tags.a(node.title.text))
-        brand["class"] = "navbar-brand"
-        brand["href"] = node.title.get_url()
+        a = div_header.add(
+            tags.a(cls="navbar-brand", href=node.title.get_url())
+        )
+        a.add(node.title.text)
 
-        # div for collapsible navbar items
-        # match ``node_id`` to the collapsible button defined above
-        bar = cont.add(tags.div(_class="navbar-collapse collapse", id=node_id))
+        collapsible_div = container.add(
+            tags.div(cls="navbar-collapse collapse", id=node_id)
+        )
 
-        # navbar items
-        bar_list = bar.add(tags.ul(_class="nav navbar-nav"))
-        bar_list.add(*[self.visit(i) for i in node.items])
+        ul = collapsible_div.add(tags.ul(cls="nav navbar-nav"))
+        ul.add(*[self.visit(i) for i in node.items])
 
         # navbar items to the right
-        right_bar_list = bar.add(tags.ul(_class="nav navbar-nav navbar-right"))
-        right_bar_list.add(*[self.visit(i) for i in node.right_items])
+        ul_right = collapsible_div.add(
+            tags.ul(cls="nav navbar-nav navbar-right")
+        )
+        ul_right.add(*[self.visit(i) for i in node.right_items])
 
         # return the configured <nav> element
-        return root
+        return nav
 
     @staticmethod
     def visit_BadgedView(node: BadgedView) -> html_tag:
@@ -208,18 +211,16 @@ class NavbarRenderer(Visitor):
         :return: An ``html_tag`` instance for rendering a list object
             in an unordered list.
         """
-        item = tags.li()
-        anchor = item.add(tags.a(node.text))
-        anchor["title"] = node.title
-        anchor["href"] = node.get_url()
-        anchor["class"] = node.aclass
+        li = tags.li()
+        a = li.add(tags.a(cls=node.cls, href=node.get_url(), title=node.title))
+        a.add(node.text)
 
         # create the badge that will display the number of focused items
         # if the node count is not 0
         if node.count:
-            anchor.add(node.badge)
+            a.add(node.badge)
 
-        return item
+        return li
 
     def visit_ListGroup(self, node: ListGroup) -> html_tag:
         """A simpler form of ``visit_Subgroup``.
@@ -230,10 +231,10 @@ class NavbarRenderer(Visitor):
         :return: An ``html_tag`` instance for rendering a list object
             in an unordered list.
         """
-        item = tags.li()
-        div = item.add(tags.div(_class="list-group"))
+        li = tags.li()
+        div = li.add(tags.div(cls="list-group"))
         div.add(*[self.visit(i) for i in node.items])
-        return item
+        return li
 
     @staticmethod
     def visit_Toggler(node: Toggler) -> html_tag:
@@ -252,12 +253,10 @@ class NavbarRenderer(Visitor):
         :return: An ``html_tag`` instance for rendering a list object
             in an unordered list.
         """
-        item = tags.li()
-        anchor = item.add(tags.a(node.text))
-        anchor["href"] = node.get_url()
-        anchor["title"] = node.title
-        anchor["class"] = node.aclass
-        return item
+        li = tags.li()
+        a = li.add(tags.a(cls=node.cls, href=node.get_url(), title=node.title))
+        a.add(node.text)
+        return li
 
     # all the below is from `Flask-Bootstrap.nav.BootstrapRenderer`
     # cannot be installed at the same time as `Bootstrap-Flask`
@@ -269,21 +268,25 @@ class NavbarRenderer(Visitor):
 
         :return: An ``html_tag`` instance for rendering a subgroup.
         """
-        li = tags.li()
+        li = tags.li(cls="dropdown")
         if not self._in_dropdown:
-            li["class"] = "dropdown"
-            a = li.add(tags.a(node.title, href="#", _class="dropdown-toggle"))
-            a["data-toggle"] = "dropdown"
-            a["role"] = "button"
-            a["aria-haspopup"] = "true"
-            a["aria-expanded"] = "false"
-            a.add(tags.span(_class="caret"))
+            a = li.add(
+                tags.a(
+                    href="#",
+                    cls="dropdown-toggle",
+                    data_toggle="dropdown",
+                    role="button",
+                    aria_haspopup="true",
+                    aria_expanded="false",
+                )
+            )
+            a.add(node.title, tags.span(cls="caret"))
 
-            ul = li.add(tags.ul(_class="dropdown-menu"))
-
+            ul = li.add(tags.ul(cls="dropdown-menu"))
             self._in_dropdown = True
             for item in node.items:
                 ul.add(self.visit(item))
+
             self._in_dropdown = False
 
         return li
@@ -294,9 +297,10 @@ class NavbarRenderer(Visitor):
 
         :return: An ``html_tag`` instance for rendering a view.
         """
-        item = tags.li()
-        item.add(tags.a(node.text, href=node.get_url(), title=node.text))
+        li = tags.li()
+        a = li.add(tags.a(href=node.get_url(), title=node.text))
+        a.add(node.text)
         if node.active:
-            item["class"] = "active"
+            li["class"] = "active"
 
-        return item
+        return li
