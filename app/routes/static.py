fc363634723a0d730d8efc295e57d974f02e7d50
jshwi <stephen@jshwisolutions.com>
2021-11-21 23:54:50 +1100

    add: Adds additional favicon types

--- app/routes/static.py
+++ app/routes/static.py
@@ -15,4 +15,67 @@ def favicon_ico() -> Response:
 
     :return: Response object.
     """
-    return send_from.image("favicon.ico", image_type="png")
+    return send_from.image("favicon.ico", image_type="vnd.microsoft.icon")
+
+
+@blueprint.route("/android-chrome-192x192.png")
+def android_chrome_192x192_png() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("android-chrome-192x192.png")
+
+
+@blueprint.route("/android-chrome-512x512.png")
+def android_chrome_512x512_png() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("android-chrome-512x512.png")
+
+
+@blueprint.route("/apple-touch-icon.png")
+def apple_touch_icon_png() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("apple-touch-icon.png")
+
+
+@blueprint.route("/favicon-16x16.png")
+def favicon_16x16_png() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("favicon-16x16.png")
+
+
+@blueprint.route("/favicon-32x32.png")
+def favicon_32x32_png() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("favicon-32x32.png")
+
+
+@blueprint.route("/mstile-150x150.png")
+def mstile_150x150_png() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("mstile-150x150.png")
+
+
+@blueprint.route("/safari-pinned-tab.svg")
+def safari_pinned_tab_svg() -> Response:
+    """Serve image to the root of the application.
+
+    :return: Response object.
+    """
+    return send_from.image("safari-pinned-tab.svg", image_type="svg+xml")
--- tests/utils.py
+++ tests/utils.py
@@ -144,6 +144,13 @@ STATUS_CODE_TO_ROUTE_DEFAULT = [
             "/auth/logout",
             "/auth/reset_password/<token>",
             "/favicon.ico",
+            "/android-chrome-192x192.png",
+            "/android-chrome-512x512.png",
+            "/apple-touch-icon.png",
+            "/favicon-16x16.png",
+            "/favicon-32x32.png",
+            "/mstile-150x150.png",
+            "/safari-pinned-tab.svg",
         ],
     ),
     (
