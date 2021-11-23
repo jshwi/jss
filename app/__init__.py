01e86f5a43c32e887d57c779166ab98f0c57576e
jshwi <stephen@jshwisolutions.com>
2021-11-24 01:48:14 +1100

    Bump version: 1.12.0 → 1.12.1

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,7 @@ from app import (
     shell,
 )
 
-__version__ = "1.12.0"
+__version__ = "1.12.1"
 
 
 def create_app() -> Flask:
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.rst") as file:
 
 setup(
     name="jss",
-    version="1.12.0",
+    version="1.12.1",
     description="A Flask web application",
     long_description=README,
     long_description_content_type="text/x-rst",
