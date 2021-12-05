b6d225a339b1dad2c1b17da7ca72f2a37ab44c3a
jshwi <stephen@jshwisolutions.com>
2021-12-06 01:07:26 +1100

    Bump version: 1.13.0 → 1.13.1

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,7 @@ from app import (
     shell,
 )
 
-__version__ = "1.13.0"
+__version__ = "1.13.1"
 
 
 def create_app() -> Flask:
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "1.13.0"
+release = "1.13.1"
 
 
 # -- General configuration ---------------------------------------------------
