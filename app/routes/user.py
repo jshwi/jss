c4a5a08ccd20df82da6043f34643be6a66f70487
jshwi <stephen@jshwisolutions.com>
2022-12-29 14:46:53 +1100

    refactor: Add `app.constants`
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- app/routes/user.py
+++ app/routes/user.py
@@ -18,6 +18,7 @@ from flask import (
 from flask_login import current_user, login_required
 from werkzeug import Response
 
+from app.constants import GET, POST
 from app.forms import EditProfile, MessageForm
 from app.models import Message, Notification, User, Usernames, db
 from app.utils import redirect
@@ -26,7 +27,7 @@ from app.utils.security import confirmation_required
 blueprint = Blueprint("user", __name__, url_prefix="/user")
 
 
-@blueprint.route("/profile/edit", methods=["GET", "POST"])
+@blueprint.route("/profile/edit", methods=[GET, POST])
 @login_required
 @confirmation_required
 def edit_profile() -> str | Response:
@@ -58,7 +59,7 @@ def edit_profile() -> str | Response:
     return render_template("user/edit_profile.html", form=form)
 
 
-@blueprint.route("/send_message/<recipient>", methods=["GET", "POST"])
+@blueprint.route("/send_message/<recipient>", methods=[GET, POST])
 @login_required
 @confirmation_required
 def send_message(recipient: str) -> str | Response:
--- app/utils/lang.py
+++ app/utils/lang.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 from flask import current_app
 
+from app.constants import COPYRIGHT_AUTHOR, ENCODING, TRANSLATIONS_DIR
 from app.version import __version__
 
 MAPPING = """
@@ -46,18 +47,18 @@ def _strip_extra_newline(path: Path) -> None:
 def _add_translator() -> None:
     pot_file = _pot_file()
     pot_file.write_text(
-        pot_file.read_text(encoding="utf-8")
-        .replace("FIRST AUTHOR", current_app.config["COPYRIGHT_AUTHOR"])
-        .replace("FULL NAME", current_app.config["COPYRIGHT_AUTHOR"])
+        pot_file.read_text(encoding=ENCODING)
+        .replace("FIRST AUTHOR", current_app.config[COPYRIGHT_AUTHOR])
+        .replace("FULL NAME", current_app.config[COPYRIGHT_AUTHOR])
         .replace("EMAIL@ADDRESS", current_app.config["COPYRIGHT_EMAIL"]),
-        encoding="utf-8",
+        encoding=ENCODING,
     )
 
 
 def _pybabel_extract() -> None:
     with tempfile.NamedTemporaryFile() as tmp:
         mapping_file = Path(tmp.name)
-        mapping_file.write_text(MAPPING, encoding="utf-8")
+        mapping_file.write_text(MAPPING, encoding=ENCODING)
         _pybabel(
             "extract",
             "--keywords=_l",
@@ -65,7 +66,7 @@ def _pybabel_extract() -> None:
             "--msgid-bugs-address",
             current_app.config["COPYRIGHT_EMAIL"],
             "--copyright-holder",
-            current_app.config["COPYRIGHT_AUTHOR"],
+            current_app.config[COPYRIGHT_AUTHOR],
             "--project",
             Path.cwd().name,
             "--version",
@@ -85,12 +86,12 @@ def _pybabel_update() -> None:
         "--input-file",
         _pot_file(),
         "--output-dir",
-        current_app.config["TRANSLATIONS_DIR"],
+        current_app.config[TRANSLATIONS_DIR],
     )
 
 
 def _pybabel_compile() -> None:
-    _pybabel("compile", "--directory", current_app.config["TRANSLATIONS_DIR"])
+    _pybabel("compile", "--directory", current_app.config[TRANSLATIONS_DIR])
 
 
 def _pybabel_init(lang: str) -> None:
@@ -99,7 +100,7 @@ def _pybabel_init(lang: str) -> None:
         "--input-file",
         _pot_file(),
         "--output-dir",
-        current_app.config["TRANSLATIONS_DIR"],
+        current_app.config[TRANSLATIONS_DIR],
         "--locale",
         lang,
     )
@@ -109,7 +110,7 @@ def translate_update_cli() -> None:
     """Update all languages."""
     _pybabel_extract()
     _pybabel_update()
-    for lang in current_app.config["TRANSLATIONS_DIR"].iterdir():
+    for lang in current_app.config[TRANSLATIONS_DIR].iterdir():
         _strip_extra_newline(lang / _lc_dir() / _po_file())
 
     os.remove(_pot_file())
@@ -117,9 +118,9 @@ def translate_update_cli() -> None:
 
 def translate_compile_cli() -> None:
     """Compile all languages."""
-    tdir: Path = current_app.config["TRANSLATIONS_DIR"]
+    tdir: Path = current_app.config[TRANSLATIONS_DIR]
     if tdir.is_dir():
-        for lang in current_app.config["TRANSLATIONS_DIR"].iterdir():
+        for lang in current_app.config[TRANSLATIONS_DIR].iterdir():
             catalog = lang / _lc_dir() / _po_file()
             if catalog.is_file():
                 _pybabel_compile()
@@ -136,6 +137,6 @@ def translate_init_cli(lang: str) -> None:
     _pybabel_extract()
     _pybabel_init(lang)
     _strip_extra_newline(
-        current_app.config["TRANSLATIONS_DIR"] / lang / _lc_dir() / _po_file()
+        current_app.config[TRANSLATIONS_DIR] / lang / _lc_dir() / _po_file()
     )
     os.remove(_pot_file())
