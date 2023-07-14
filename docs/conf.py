d6d90ee527d1c54553a1d0303e3640fa0ae778c4
jshwi <stephen@jshwisolutions.com>
2023-07-14 20:39:36 +1000

    bump: version 1.24.0 → 1.24.1
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "1.24.0"
+release = "1.24.1"
 
 
 # -- General configuration ---------------------------------------------------
