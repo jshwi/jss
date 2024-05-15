4fa7f7b505a35fd4d6cb576242b5bf03b645d7fd
Stephen Whitlock <stephen@jshwisolutions.com>
2024-05-15 10:05:10 +1000

    bump: version 1.36.1 → 1.36.2
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "1.36.1"
+release = "1.36.2"
 
 
 # -- General configuration ---------------------------------------------------
